7c63edce727de2d45d66b30c45c4ac696e822d21
Vedanshu Sharma <vedan987@gmail.com>
2023-06-18 18:29:43 +0530

    Update Security changes  utils.py

--- home/utils.py
+++ home/utils.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 
 def get_db_handle():
-    client = MongoClient('mongodb+srv://vedan987:Vedanshu1502@cluster-books.jailwop.mongodb.net/?retryWrites=true&w=majority')
+    client = MongoClient('mongodb+srv://vedan987:<passwd>@cluster-books.jailwop.mongodb.net/?retryWrites=true&w=majority')
     db_handle = client['Books-Data']
     return db_handle, client
 
